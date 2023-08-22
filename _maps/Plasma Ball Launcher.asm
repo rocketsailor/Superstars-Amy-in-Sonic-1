0d1082d7f164c0bb15f2b4d42d1ae8931307493c
emma <130490422+rocketsailor@users.noreply.github.com>
2023-08-21 21:02:00 -0700

    Eggman and other color correction
    Just color correction for some things, mostly Eggman

--- _maps/Plasma Ball Launcher.asm
+++ _maps/Plasma Ball Launcher.asm
@@ -1,17 +1,15 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - energy ball	launcher (FZ)
-; ---------------------------------------------------------------------------
-Map_PLaunch_internal:
-		dc.w .red-Map_PLaunch_internal
-		dc.w .white-Map_PLaunch_internal
-		dc.w .sparking1-Map_PLaunch_internal
-		dc.w .sparking2-Map_PLaunch_internal
-.red:		dc.b 1
-		dc.b $F8, 5, 0,	$6E, $F8
-.white:		dc.b 1
-		dc.b $F8, 5, 0,	$76, $F8
-.sparking1:	dc.b 1
-		dc.b $F8, 5, 0,	$72, $F8
-.sparking2:	dc.b 1
-		dc.b $F8, 5, $10, $72, $F8
-		even
+Map_5663:
+Map_5663_0: 	dc.w Map_5663_8-Map_5663
+Map_5663_2: 	dc.w Map_5663_E-Map_5663
+Map_5663_4: 	dc.w Map_5663_19-Map_5663
+Map_5663_6: 	dc.w Map_5663_1F-Map_5663
+Map_5663_8: 	dc.b $1
+	dc.b $F8, $5, $40, $6E, $F8
+Map_5663_E: 	dc.b $2
+	dc.b $F8, $5, $40, $76, $F8
+	dc.b $0, $0, $0, $0, $0
+Map_5663_19: 	dc.b $1
+	dc.b $F8, $5, $40, $72, $F8
+Map_5663_1F: 	dc.b $1
+	dc.b $F8, $5, $50, $72, $F8
+	even
